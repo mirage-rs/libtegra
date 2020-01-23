700a9796f3923343d0417a61c2de3d84b0440b54
Valentin B <valentin.be@protonmail.com>
2020-01-23 16:33:46 +0100

    Add Uart#flush and fix a bug where Uart#init would spin forever

--- src/uart/mod.rs
+++ src/uart/mod.rs
@@ -1,14 +1,31 @@
-use core::{fmt::{Write, Error}, marker::Sync};
+use core::{
+    cell::Cell,
+    fmt::{Write, Error},
+    marker::Sync,
+};
 
 use crate::{car::Clock, timer::usleep};
 
-use registers::*;
+pub use registers::*;
 
 mod registers;
 
 /// Representation of a UART.
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Uart {
+    /// The baud rate this [`Uart`] is configured with.
+    ///
+    /// Use [`Uart::get_baud_rate`] to obtain the "real" rate
+    /// the Uart is configured with.
+    ///
+    /// NOTE: This value will be overridden by [`Uart::init`],
+    /// so it is safe to initialize instances of this struct
+    /// with a dummy value, such as `Cell::new(0)`.
+    ///
+    /// [`Uart`]: struct.Uart.html
+    /// [`Uart::get_baud_rate`]: struct.Uart.html#method.get_baud_rate
+    /// [`Uart::init`]: struct.Uart.html#method.init
+    baud: Cell<u32>,
     /// A reference to the device clock that corresponds to the UART.
     clock: &'static Clock,
     /// A pointer to the [`Registers`] of the UART.
@@ -22,30 +39,35 @@ pub struct Uart {
 impl Uart {
     /// Representation of UART A.
     pub const A: Self = Uart {
+        baud: Cell::new(0),
         clock: &Clock::UART_A,
         registers: UART_A_REGISTERS,
     };
 
     /// Representation of UART B.
     pub const B: Self = Uart {
+        baud: Cell::new(0),
         clock: &Clock::UART_B,
         registers: UART_B_REGISTERS,
     };
 
     /// Representation of UART C.
     pub const C: Self = Uart {
+        baud: Cell::new(0),
         clock: &Clock::UART_C,
         registers: UART_C_REGISTERS,
     };
 
     /// Representation of UART D.
     pub const D: Self = Uart {
+        baud: Cell::new(0),
         clock: &Clock::UART_D,
         registers: UART_D_REGISTERS,
     };
 
     /// Representation of UART E.
     pub const E: Self = Uart {
+        baud: Cell::new(0),
         clock: &Clock::UART_APE,
         registers: UART_E_REGISTERS,
     };
@@ -53,17 +75,28 @@ impl Uart {
 
 impl Uart {
     #[inline(always)]
-    fn wait_cycles(&self, baud_rate: u32, amount: u32) {
+    fn wait_cycles(&self, amount: u32) {
+        let baud_rate = self.baud.get();
         usleep((amount * 1_000_000 + 16 * baud_rate - 1) / (16 * baud_rate));
     }
 
     #[inline(always)]
-    fn wait_symbols(&self, baud_rate: u32, amount: u32) {
+    fn wait_symbols(&self, amount: u32) {
+        let baud_rate = self.baud.get();
         usleep((amount * 1_000_000 + baud_rate - 1) / baud_rate);
+    }
+
+    pub fn get_baud_rate(&self) -> u32 {
+        let baud_rate = self.baud.get();
+
+        (8 * baud_rate + 408_000_000) / (16 * baud_rate)
     }
 
     pub fn init(&self, baud_rate: u32) {
         let controller = unsafe { &*self.registers };
+
+        // Store the provided baud rate.
+        self.baud.set(baud_rate);
 
         // Enable the device clock.
         self.clock.enable();
@@ -73,26 +106,26 @@ impl Uart {
         }
 
         // Calculate the baud rate, rounded to nearest.
-        let rate = (8 * baud_rate + 408_000_000) / (16 * baud_rate);
+        let real_baud_rate = self.get_baud_rate();
 
         // Setup UART in FIFO mode.
 
         // Disable interrupts.
         controller.UART_IER_DLAB_0_0.set(0);
-        // No hardware flow control.
+        // Disable hardware flow control.
         controller.UART_MCR_0.set(0);
         // Enable DLAB and set word length to 8.
         controller.UART_LCR_0.modify(UART_LCR_0::DLAB::SET + UART_LCR_0::WD_SIZE::WordLength8);
         // Divisor latch LSB.
-        controller.UART_THR_DLAB_0_0.set(rate);
+        controller.UART_THR_DLAB_0_0.set(real_baud_rate);
         // Divisor latch MSB.
-        controller.UART_IER_DLAB_0_0.set(rate >> 8);
+        controller.UART_IER_DLAB_0_0.set(real_baud_rate >> 8);
         // Disable DLAB.
         controller.UART_LCR_0.modify(UART_LCR_0::DLAB::CLEAR);
         // Dummy read.
         controller.UART_SPR_0.get();
         // Wait 3 symbols for the new baud rate.
-        self.wait_symbols(baud_rate, 3);
+        self.wait_symbols(3);
 
         // Enable FIFO with default settings.
 
@@ -101,22 +134,10 @@ impl Uart {
         // Dummy read.
         controller.UART_SPR_0.get();
         // Wait for 3 baud cycles.
-        self.wait_cycles(baud_rate, 3);
+        self.wait_cycles(3);
 
-        // Flush the FIFO.
-
-        while !controller.UART_LSR_0.is_set(UART_LSR_0::TMTY) {
-            // Make sure there is no data being written to TX FIFO.
-        }
-        // Clear TX and RX FIFOs.
-        controller.UART_IIR_FCR_0.modify(UART_IIR_FCR_0::TX_CLR::SET + UART_IIR_FCR_0::RX_CLR::SET);
-        // Wait for 32 baud cycles.
-        self.wait_cycles(baud_rate, 32);
-        while !controller.UART_LSR_0.is_set(UART_LSR_0::TMTY)
-            && !controller.UART_LSR_0.is_set(UART_LSR_0::RDR)
-        {
-            // Wait until the FIFOs are ready.
-        }
+        // Flush the FIFOs and wait until they are ready.
+        self.flush();
     }
 
     pub fn read_byte(&self) -> u8 {
@@ -164,6 +185,35 @@ impl Uart {
 
         while !controller.UART_LSR_0.is_set(UART_LSR_0::THRE) {
             // Wait for everything to be written.
+        }
+    }
+
+    pub fn flush(&self) {
+        let controller = unsafe { &*self.registers };
+
+        while !controller.UART_LSR_0.is_set(UART_LSR_0::TMTY) {
+            // Make sure there is no data being written to TX FIFO.
+        }
+
+        // Disable hardware control flow.
+        controller.UART_MCR_0.set(0);
+        // Dummy read.
+        controller.UART_SPR_0.get();
+        // Wait for 1 character time.
+        // XXX: Figure out how to calculate this from code.
+        usleep(96);
+
+        // Issue flush requests for TX FIFO and RX FIFO.
+        controller.UART_IIR_FCR_0.modify(UART_IIR_FCR_0::TX_CLR::SET + UART_IIR_FCR_0::RX_CLR::SET);
+        // Dummy read.
+        controller.UART_SPR_0.get();
+        // Wait for 32 baud cycles.
+        self.wait_cycles(32);
+
+        while !controller.UART_LSR_0.is_set(UART_LSR_0::TMTY)
+            && controller.UART_LSR_0.is_set(UART_LSR_0::RDR)
+        {
+            // Wait until the FIFOs are ready.
         }
     }
 }

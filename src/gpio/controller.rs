ef54953c90cfe2e7565908b5201881893e2b41ca
Valentin B <valentin.be@protonmail.com>
2020-03-09 21:41:30 +0100

    Utilize register_structs! for GpioBank registers

--- src/gpio/controller.rs
+++ src/gpio/controller.rs
@@ -3,7 +3,7 @@
 //! See Chapter 9.13 in the Tegra X1 Technical Reference Manual for
 //! details.
 
-use register::mmio::ReadWrite;
+use register::{mmio::ReadWrite, register_structs};
 
 use crate::memory_map::gpio::BASE;
 
@@ -18,26 +18,28 @@ const GPIO_BANKS_COUNT: usize = 8;
 
 // TODO: Bitfields for the registers?
 
-/// Representation of a GPIO bank.
-#[allow(non_snake_case)]
-#[repr(C)]
-pub struct GpioBank {
-    pub GPIO_CONFIG: [ReadWrite<u32>; GPIO_PORTS_COUNT],
-    pub GPIO_OUTPUT_ENABLE: [ReadWrite<u32>; GPIO_PORTS_COUNT],
-    pub GPIO_OUT: [ReadWrite<u32>; GPIO_PORTS_COUNT],
-    pub GPIO_IN: [ReadWrite<u32>; GPIO_PORTS_COUNT],
-    pub GPIO_INT_STATUS: [ReadWrite<u32>; GPIO_PORTS_COUNT],
-    pub GPIO_INT_ENABLE: [ReadWrite<u32>; GPIO_PORTS_COUNT],
-    pub GPIO_INT_LEVEL: [ReadWrite<u32>; GPIO_PORTS_COUNT],
-    pub GPIO_INT_CLEAR: [ReadWrite<u32>; GPIO_PORTS_COUNT],
-    pub GPIO_MASKED_CONFIG: [ReadWrite<u32>; GPIO_PORTS_COUNT],
-    pub GPIO_MASKED_OUTPUT_ENABLE: [ReadWrite<u32>; GPIO_PORTS_COUNT],
-    pub GPIO_MASKED_OUT: [ReadWrite<u32>; GPIO_PORTS_COUNT],
-    pub GPIO_MASKED_IN: [ReadWrite<u32>; GPIO_PORTS_COUNT],
-    pub GPIO_MASKED_INT_STATUS: [ReadWrite<u32>; GPIO_PORTS_COUNT],
-    pub GPIO_MASKED_INT_ENABLE: [ReadWrite<u32>; GPIO_PORTS_COUNT],
-    pub GPIO_MASKED_INT_LEVEL: [ReadWrite<u32>; GPIO_PORTS_COUNT],
-    pub GPIO_MASKED_INT_CLEAR: [ReadWrite<u32>; GPIO_PORTS_COUNT],
+register_structs! {
+    /// Representation of a GPIO bank.
+    #[allow(non_snake_case)]
+    pub GpioBank {
+        (0x000 => pub GPIO_CONFIG: [ReadWrite<u32>; GPIO_PORTS_COUNT]),
+        (0x010 => pub GPIO_OUTPUT_ENABLE: [ReadWrite<u32>; GPIO_PORTS_COUNT]),
+        (0x020 => pub GPIO_OUT: [ReadWrite<u32>; GPIO_PORTS_COUNT]),
+        (0x030 => pub GPIO_IN: [ReadWrite<u32>; GPIO_PORTS_COUNT]),
+        (0x040 => pub GPIO_INT_STATUS: [ReadWrite<u32>; GPIO_PORTS_COUNT]),
+        (0x050 => pub GPIO_INT_ENABLE: [ReadWrite<u32>; GPIO_PORTS_COUNT]),
+        (0x060 => pub GPIO_INT_LEVEL: [ReadWrite<u32>; GPIO_PORTS_COUNT]),
+        (0x070 => pub GPIO_INT_CLEAR: [ReadWrite<u32>; GPIO_PORTS_COUNT]),
+        (0x080 => pub GPIO_MASKED_CONFIG: [ReadWrite<u32>; GPIO_PORTS_COUNT]),
+        (0x090 => pub GPIO_MASKED_OUTPUT_ENABLE: [ReadWrite<u32>; GPIO_PORTS_COUNT]),
+        (0x0A0 => pub GPIO_MASKED_OUT: [ReadWrite<u32>; GPIO_PORTS_COUNT]),
+        (0x0B0 => pub GPIO_MASKED_IN: [ReadWrite<u32>; GPIO_PORTS_COUNT]),
+        (0x0C0 => pub GPIO_MASKED_INT_STATUS: [ReadWrite<u32>; GPIO_PORTS_COUNT]),
+        (0x0D0 => pub GPIO_MASKED_INT_ENABLE: [ReadWrite<u32>; GPIO_PORTS_COUNT]),
+        (0x0E0 => pub GPIO_MASKED_INT_LEVEL: [ReadWrite<u32>; GPIO_PORTS_COUNT]),
+        (0x0F0 => pub GPIO_MASKED_INT_CLEAR: [ReadWrite<u32>; GPIO_PORTS_COUNT]),
+        (0x100 => @END),
+    }
 }
 
 assert_eq_size!(GpioBank, [u8; 0x100]);
@@ -45,7 +47,7 @@ assert_eq_size!(GpioBank, [u8; 0x100]);
 /// Representation of the GPIO controller.
 #[repr(C)]
 pub struct GpioController {
-    /// The GPIO banks that are part of the controller
+    /// The GPIO banks that are part of the controller.
     pub banks: [GpioBank; GPIO_BANKS_COUNT],
 }
 

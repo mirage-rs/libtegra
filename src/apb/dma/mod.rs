71e97a533c65cc7c39f726ce2e65ca4f449de7f5
Valentin B <valentin.be@protonmail.com>
2020-03-10 17:33:28 +0100

    Add definitions for the 32 DMA Channels

--- src/apb/dma/mod.rs
+++ src/apb/dma/mod.rs
@@ -38,6 +38,202 @@ pub struct Channel {
     claimed: Cell<bool>,
 }
 
+// Definitions of known Channels.
+
+impl Channel {
+    /// Representation of the DMA Channel 0.
+    pub const CH0: Self = Channel {
+        registers: CHANNEL_0,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 1.
+    pub const CH1: Self = Channel {
+        registers: CHANNEL_1,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 2.
+    pub const CH2: Self = Channel {
+        registers: CHANNEL_2,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 3.
+    pub const CH3: Self = Channel {
+        registers: CHANNEL_3,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 4.
+    pub const CH4: Self = Channel {
+        registers: CHANNEL_4,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 5.
+    pub const CH5: Self = Channel {
+        registers: CHANNEL_5,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 6.
+    pub const CH6: Self = Channel {
+        registers: CHANNEL_6,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 7.
+    pub const CH7: Self = Channel {
+        registers: CHANNEL_7,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 8.
+    pub const CH8: Self = Channel {
+        registers: CHANNEL_8,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 9.
+    pub const CH9: Self = Channel {
+        registers: CHANNEL_9,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 10.
+    pub const CH10: Self = Channel {
+        registers: CHANNEL_10,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 11.
+    pub const CH11: Self = Channel {
+        registers: CHANNEL_11,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 12.
+    pub const CH12: Self = Channel {
+        registers: CHANNEL_12,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 13.
+    pub const CH13: Self = Channel {
+        registers: CHANNEL_13,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 14.
+    pub const CH14: Self = Channel {
+        registers: CHANNEL_14,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 15.
+    pub const CH15: Self = Channel {
+        registers: CHANNEL_15,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 16.
+    pub const CH16: Self = Channel {
+        registers: CHANNEL_16,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 17.
+    pub const CH17: Self = Channel {
+        registers: CHANNEL_17,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 18.
+    pub const CH18: Self = Channel {
+        registers: CHANNEL_18,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 19.
+    pub const CH19: Self = Channel {
+        registers: CHANNEL_19,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 20.
+    pub const CH20: Self = Channel {
+        registers: CHANNEL_20,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 21.
+    pub const CH21: Self = Channel {
+        registers: CHANNEL_21,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 22.
+    pub const CH22: Self = Channel {
+        registers: CHANNEL_22,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 23.
+    pub const CH23: Self = Channel {
+        registers: CHANNEL_23,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 24.
+    pub const CH24: Self = Channel {
+        registers: CHANNEL_24,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 25.
+    pub const CH25: Self = Channel {
+        registers: CHANNEL_25,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 26.
+    pub const CH26: Self = Channel {
+        registers: CHANNEL_26,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 27.
+    pub const CH27: Self = Channel {
+        registers: CHANNEL_27,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 28.
+    pub const CH28: Self = Channel {
+        registers: CHANNEL_28,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 29.
+    pub const CH29: Self = Channel {
+        registers: CHANNEL_29,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 30.
+    pub const CH30: Self = Channel {
+        registers: CHANNEL_30,
+        claimed: Cell::new(false),
+    };
+
+    /// Representation of the DMA Channel 31.
+    pub const CH31: Self = Channel {
+        registers: CHANNEL_31,
+        claimed: Cell::new(false),
+    };
+}
+
 impl Channel {
     /// Acquires the current channel.
     ///

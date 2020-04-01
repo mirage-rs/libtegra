3ea18ffa085d436103225ebc588d154d07babae8
Justus K <justus.k@protonmail.com>
2020-04-02 00:06:31 +0200

    Implement hal traits for spi module

--- src/hal/spi.rs
+++ src/hal/spi.rs
@@ -1,1 +1,20 @@
-// TODO: Implement this
+use crate::spi::Spi;
+use embedded_hal::blocking::spi::{Write, Transfer};
+
+impl Write<u8> for Spi {
+    type Error = ();
+
+    fn write(&mut self, words: &[u8]) -> Result<(), Self::Error> {
+        Spi::write(self, words)
+    }
+}
+
+impl Transfer<u8> for Spi {
+    type Error = ();
+
+    fn transfer<'w>(&mut self, words: &'w mut [u8]) -> Result<&'w [u8], Self::Error> {
+        Spi::write(self, words)?;
+        Spi::read(self, words)?;
+        Ok(words)
+    }
+}

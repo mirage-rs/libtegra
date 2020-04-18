c320c4c522df12b8a3fc30e2bfe7c4b1d0d521e1
Valentin B <valentin.be@protonmail.com>
2020-04-18 17:37:08 +0200

    Restructure and document se module

---
+++ src/se/core.rs
@@ -0,0 +1,230 @@
+//! Implementation of the Security Engine core functionality.
+
+use core::convert::TryFrom;
+
+use crate::{ahb::mem, timer};
+
+use super::{constants::*, REGISTERS};
+
+/// Address information of a DMA buffer.
+///
+/// Effectively used as a node of a Security Engine [`LinkedList`].
+///
+/// [`LinkedList`]: struct.LinkedList.html
+#[derive(Debug, Default)]
+#[repr(C)]
+pub struct AddressInfo {
+    /// The start address of the DMA buffer.
+    pub address: u32,
+    /// The length of data stored in the DMA buffer.
+    pub data_len: u32,
+}
+
+impl<'a> From<&'a [u8]> for AddressInfo {
+    fn from(buffer: &[u8]) -> Self {
+        let address = u32::try_from(buffer.as_ptr() as usize)
+            .expect("Address does not fit an u32!");
+        let data_len = buffer.len() as u32;
+
+        AddressInfo {
+            address,
+            data_len,
+        }
+    }
+}
+
+assert_eq_size!(AddressInfo, [u32; 2]);
+
+/// Representation of a Security Engine Linked List.
+///
+/// Linked Lists are used for I/O within the Security Engine
+/// and provide the buffers of data to read input from and
+/// output the resulting data to.
+#[derive(Debug, Default)]
+#[repr(align(4), C)]
+pub struct LinkedList {
+    /// The total number of DMA entries within this list.
+    ///
+    /// NOTE: This number may not exceed 3.
+    pub entries: u32,
+    /// An array of DMA buffer information to be processed.
+    pub address_info: [AddressInfo; 0x4],
+}
+
+impl LinkedList {
+    /// Inserts a given DMA buffer into the Linked List.
+    ///
+    /// If the total capacity of the list is already exhausted,
+    /// this function will return an error.
+    pub fn append(&mut self, entry: &[u8]) -> Result<(), ()> {
+        if self.entries >= 3 {
+            return Err(());
+        }
+
+        self.entries += 1;
+        self.address_info[self.entries as usize] = AddressInfo::from(entry);
+
+        Ok(())
+    }
+}
+
+impl<'a> From<&'a [u8]> for LinkedList {
+    fn from(buffer: &[u8]) -> Self {
+        LinkedList {
+            entries: 0,
+            address_info: [
+                AddressInfo::from(buffer),
+                AddressInfo::default(),
+                AddressInfo::default(),
+                AddressInfo::default(),
+            ],
+        }
+    }
+}
+
+assert_eq_size!(LinkedList, [u32; 9]);
+
+/// Enumeration of potential Security Engine errors that
+/// may occur during internal operations on it.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum OperationError {
+    /// A timeout has occurred during a SE operation.
+    Timeout,
+    /// A timeout has occurred during an AHB transfer.
+    AhbTimeout,
+    /// An exception was raised by the SE while processing DMA buffers.
+    Exception,
+}
+
+/// Waits for the Security Engine to enter idle state before starting the next operation.
+///
+/// This function also clears pending interrupts from previous operations.
+fn prepare_operation() -> Result<(), OperationError> {
+    let engine = unsafe { &*REGISTERS };
+
+    // Disable interrupts to be issued by a Security Engine operation.
+    engine.SE_INT_ENABLE_0.set(0);
+
+    // Wait for the previous operation to finish.
+    let timeout = timer::get_milliseconds() + 100;
+    while engine.SE_STATUS_0.get() != 0 {
+        if timer::get_milliseconds() > timeout {
+            return Err(OperationError::Timeout);
+        }
+    }
+
+    // Clear any pending interrupts from the previous operation.
+    engine.SE_INT_STATUS_0.set(engine.SE_INT_STATUS_0.get());
+
+    Ok(())
+}
+
+/// Checks whether a Security Engine operation has been fully completed.
+///
+/// This function ensures that no interrupts are pending, no exceptions
+/// have occurred internally, the AHB transfer has terminated and that
+/// the Security Engine has entered idle state.
+fn complete_operation() -> Result<(), OperationError> {
+    let engine = unsafe { &*REGISTERS };
+    let ahb = unsafe { &*mem::REGISTERS };
+
+    let mut timeout;
+
+    // Ensure that the operation has completed by polling the interrupt register.
+    timeout = timer::get_milliseconds() + 100;
+    while (engine.SE_INT_STATUS_0.get() & 0x10) == 0 {
+        if timer::get_milliseconds() > timeout {
+            return Err(OperationError::Timeout);
+        }
+    }
+
+    // Ensure that the operation has completed by polling the status register.
+    timeout = timer::get_milliseconds() + 100;
+    while engine.SE_STATUS_0.get() != 0 {
+        if timer::get_milliseconds() > timeout {
+            return Err(OperationError::Timeout);
+        }
+    }
+
+    // Ensure the AHB bus transfer has completed.
+    timeout = timer::get_milliseconds() + 100;
+    while (ahb.AHB_ARBITRATION_AHB_MEM_WRQUE_MST_ID_0.get() & 0x6000) != 0 {
+        if timer::get_milliseconds() > timeout {
+            return Err(OperationError::AhbTimeout);
+        }
+    }
+
+    // Ensure that no errors occurred during the operation.
+    if engine.SE_ERR_STATUS_0.get() != 0 {
+        return Err(OperationError::Exception);
+    }
+
+    Ok(())
+}
+
+/// Launches a cryptographic Security Engine operation, given two [`LinkedList`]s for I/O.
+///
+/// [`LinkedList`]: struct.LinkedList.html
+pub fn trigger_operation(
+    opcode: u32,
+    source: &LinkedList,
+    destination: &mut LinkedList,
+    nbytes: u32,
+) -> Result<(), OperationError> {
+    let engine = unsafe { &*REGISTERS };
+
+    // Compute memory addresses of the LLs.
+    let source_address = u32::try_from(source as *const _ as usize)
+        .expect("Address does not fit an u32!");
+    let destination_address = u32::try_from(destination as *mut _ as usize)
+        .expect("Address does not fit an u32!");
+
+    // Load in the LLs.
+    engine.SE_IN_LL_ADDR_0.set(source_address);
+    engine.SE_OUT_LL_ADDR_0.set(destination_address);
+
+    // Ensure that the previous operation has fully completed.
+    prepare_operation()?;
+
+    // Program the operation size in blocks.
+    let nblocks = nbytes / aes::BLOCK_SIZE;
+    if nblocks > 0 {
+        engine.SE_CRYPTO_LAST_BLOCK_0.set(nblocks - 1);
+    }
+
+    // Start the hardware operation.
+    engine.SE_OPERATION_0.set(opcode);
+
+    // Wait for the operation to complete.
+    complete_operation()?;
+
+    Ok(())
+}
+
+/// Triggers a regular Security Engine operation.
+///
+/// See [`trigger_operation`] for further explanation.
+///
+/// [`trigger_operation`]: fn.trigger_operation.html
+#[inline(always)]
+pub fn start_normal_operation(
+    source: &LinkedList,
+    destination: &mut LinkedList,
+    nbytes: u32,
+) -> Result<(), OperationError> {
+    trigger_operation(opcodes::START, source, destination, nbytes)
+}
+
+/// Triggers a Security Engine operation that saves the crypto context afterwards.
+///
+/// See [`trigger_operation`] for further explanation.
+///
+/// [`trigger_operation`]: fn.trigger_operation.html
+#[inline(always)]
+pub fn start_context_save_operation(
+    source: &LinkedList,
+    destination: &mut LinkedList,
+    nbytes: u32,
+) -> Result<(), OperationError> {
+    trigger_operation(opcodes::CTX_SAVE, source, destination, nbytes)
+}
--- src/se/mod.rs
+++ src/se/mod.rs
@@ -1,227 +1,32 @@
-use core::convert::TryFrom;
+//! Driver for the Tegra X1 Security Engine.
+//!
+//! # Description
+//!
+//! The Security Engine is responsible for performing, cryptographic operations
+//! in a secure, hardware-based environment. Communication is done over [`LinkedList`]s,
+//! a data structure defined by the SE interface, which provides I/O buffers to
+//! operate on.
+//!
+//! ## Hardware Operations
+//!
+//! As mentioned previously, the Security Engine performs hardware-based operations
+//! to process DMA buffers of data. These operations can be performed through the
+//! [`trigger_operation`] function.
+//!
+//! As this is a low-level interface that should only be used if no other possibility
+//! exists, there are higher-level wrappers around commonly used operations available:
+//!
+//! ```no_run
+//! // TODO
+//! ```
+//!
+//! [`LinkedList`]: struct.LinkedList.html
+//! [`trigger_operation`]: fn.trigger_operation.html
 
-use crate::{ahb::mem, timer};
-
-use constants::*;
-
+pub use self::core::*;
 pub use registers::*;
 
 #[allow(dead_code)]
 mod constants;
+mod core;
 mod registers;
-
-/// Address information of a DMA buffer, representing a node of the Security Engine Linked List.
-#[derive(Debug, Default)]
-#[repr(C)]
-struct AddressInfo {
-    /// The DMA buffer address.
-    pub address: u32,
-    /// The data length stored in DMA buffer.
-    pub data_len: u32,
-}
-
-impl<'a> From<&'a [u8]> for AddressInfo {
-    fn from(buffer: &[u8]) -> Self {
-        let address = u32::try_from(buffer.as_ptr() as usize)
-            .expect("Address does not fit an u32!");
-        let data_len = buffer.len() as u32;
-
-        AddressInfo {
-            address,
-            data_len,
-        }
-    }
-}
-
-assert_eq_size!(AddressInfo, [u32; 2]);
-
-/// Representation of the Security Engine Linked List.
-#[derive(Debug)]
-#[repr(align(4), C)]
-struct LinkedList {
-    /// The total number of entries.
-    pub entries: u32,
-    /// An array of DMA buffer information to be processed.
-    pub address_info: [AddressInfo; 0x4],
-}
-
-impl LinkedList {
-    /// Inserts another DMA buffer into the linked list so it can be processed.
-    ///
-    /// If the size of the list has already reached its limits, this function
-    /// will return an error.
-    pub fn append(&mut self, entry: &[u8]) -> Result<(), ()> {
-        if self.entries >= 3 {
-            return Err(());
-        }
-
-        self.entries += 1;
-        self.address_info[self.entries as usize] = AddressInfo::from(entry);
-
-        Ok(())
-    }
-}
-
-impl<'a> From<&'a [u8]> for LinkedList {
-    fn from(buffer: &[u8]) -> Self {
-        LinkedList {
-            entries: 0,
-            address_info: [
-                AddressInfo::from(buffer),
-                AddressInfo::default(),
-                AddressInfo::default(),
-                AddressInfo::default(),
-            ],
-        }
-    }
-}
-
-assert_eq_size!(LinkedList, [u32; 9]);
-
-/// Enumeration of potential Security Engine errors
-/// that may occur during internal operations on it.
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum OperationError {
-    /// A timeout has occurred during an operation.
-    Timeout,
-    /// A timeout has occurred during an AHB transfer.
-    AhbTimeout,
-    /// An exception was raised during an operation.
-    Exception,
-}
-
-/// Waits for the Security Engine to enter idle state before starting the next operation.
-///
-/// This function also clears pending interrupts from previous operations.
-/// In case of a timeout, an error will be returned.
-fn prepare_operation() -> Result<(), OperationError> {
-    let engine = unsafe { &*REGISTERS };
-
-    // Disable interrupts to be issued by a Security Engine operation.
-    engine.SE_INT_ENABLE_0.set(0);
-
-    // Wait for the previous operation to finish.
-    let timeout = timer::get_milliseconds() + 100;
-    while engine.SE_STATUS_0.get() != 0 {
-        if timer::get_milliseconds() > timeout {
-            return Err(OperationError::Timeout);
-        }
-    }
-
-    // Clear any pending interrupts from the previous operation.
-    engine.SE_INT_STATUS_0.set(engine.SE_INT_STATUS_0.get());
-
-    Ok(())
-}
-
-/// Checks that a Security Engine operation has been fully completed after kickoff.
-///
-/// This function ensures that no interrupts are pending, no exceptions have occurred,
-/// the AHB transfer has terminated and that the Security Engine has entered idle state.
-fn complete_operation() -> Result<(), OperationError> {
-    let engine = unsafe { &*REGISTERS };
-    let ahb = unsafe { &*mem::REGISTERS };
-    let mut timeout;
-
-    // Poll the interrupt register to ensure the operation has completed.
-    timeout = timer::get_milliseconds() + 100;
-    while (engine.SE_INT_STATUS_0.get() & 0x10) == 0 {
-        if timer::get_milliseconds() > timeout {
-            return Err(OperationError::Timeout);
-        }
-    }
-
-    // Poll the status register to ensure the operation has completed.
-    timeout = timer::get_milliseconds() + 100;
-    while engine.SE_STATUS_0.get() != 0 {
-        if timer::get_milliseconds() > timeout {
-            return Err(OperationError::Timeout);
-        }
-    }
-
-    // Ensure the AHB Bus transfer has completed.
-    timeout = timer::get_milliseconds() + 100;
-    while (ahb.AHB_ARBITRATION_AHB_MEM_WRQUE_MST_ID_0.get() & 0x6000) != 0 {
-        if timer::get_milliseconds() > timeout {
-            return Err(OperationError::AhbTimeout);
-        }
-    }
-
-    // Ensure that no errors occurred during the operation.
-    if engine.SE_ERR_STATUS_0.get() != 0 {
-        return Err(OperationError::Exception);
-    }
-
-    Ok(())
-}
-
-/// Starts an internal Security Engine operation.
-///
-/// Given an opcode, two [`LinkedList`]s holding data buffers and the
-/// actual amount of bytes to be processed, this function triggers
-/// a cryptographic operation within the Security Engine.
-///
-/// [`LinkedList`]: struct.LinkedList.html
-fn trigger_operation(
-    opcode: u32,
-    source: &LinkedList,
-    destination: &LinkedList,
-    nbytes: u32,
-) -> Result<(), OperationError> {
-    let engine = unsafe { &*REGISTERS };
-
-    // Compute memory addresses of the linked lists.
-    let source_address = u32::try_from(source as *const LinkedList as usize)
-        .expect("Address does not fit an u32!");
-    let destination_address = u32::try_from(destination as *const LinkedList as usize)
-        .expect("Address does not fit an u32!");
-
-    // Calculate the amount of blocks to be processed.
-    let nblocks = nbytes / aes::BLOCK_SIZE;
-
-    // Load in the linked lists for input and output.
-    engine.SE_IN_LL_ADDR_0.set(source_address);
-    engine.SE_OUT_LL_ADDR_0.set(destination_address);
-
-    // Check that the previous operation has completed.
-    prepare_operation()?;
-
-    // Program SE operation size.
-    if nblocks > 0 {
-        engine.SE_CRYPTO_LAST_BLOCK_0.set(nblocks - 1);
-    }
-
-    // Start hardware operation.
-    engine.SE_OPERATION_0.set(opcode);
-
-    // Wait for operation to complete.
-    complete_operation()?;
-
-    Ok(())
-}
-
-/// Triggers a normal Security Engine operation.
-///
-/// See [`trigger_operation`] for further explanation.
-///
-/// [`trigger_operation`]: fn.trigger_operation.html
-fn start_normal_operation(
-    source: &LinkedList,
-    destination: &LinkedList,
-    nbytes: u32
-) -> Result<(), OperationError> {
-    trigger_operation(opcodes::START, source, destination, nbytes)
-}
-
-/// Triggers a Security Engine operation where the crypto context will be saved afterwards.
-///
-/// See [`trigger_operation`] for further explanation.
-///
-/// [`trigger_operation`]: fn.trigger_operation.html
-fn start_context_save_operation(
-    source: &LinkedList,
-    destination: &LinkedList,
-    nbytes: u32
-) -> Result<(), OperationError> {
-    trigger_operation(opcodes::CTX_SAVE, source, destination, nbytes)
-}

2ef577d77b4a661dba8c7bdef3500bfcd304922f
Valentin B <valentin.be@protonmail.com>
2020-10-24 15:53:42 +0200

    Add some missing PWM docs

--- src/pwm/mod.rs
+++ src/pwm/mod.rs
@@ -1,12 +1,11 @@
-//! Driver for the PWM Controller of the Tegra X1.
+//! Driver for the Pulse Width Modulator Controller of the Tegra X1.
 //!
-//! See Chapter 39 in the Tegra X1 Technical Reference Manual
-//! for details.
+//! See Chapter 39 in the Tegra X1 Technical Reference Manual for details.
 //!
 //! # Description
-//! //! The Pulse Width Modulator (PWM) Controller is a four-channel
-//! frequency divider whose pulse width varies. Each channel has
-//! a programmable frequency divider and a programmable pulse
+//!
+//! The Pulse Width Modulator (PWM) Controller is a four-channel frequency divider whose pulse
+//! width varies. Each channel has a programmable frequency divider and a programmable pulse
 //! width generator.
 
 mod registers;
@@ -18,40 +17,41 @@ pub use crate::pwm::registers::*;
 
 /// Representation of a Pulse Width Modulator channel.
 ///
-/// NOTE: It is expected that the PWM [`Clock`] is up
-/// before using any of the functionality of any channel.
+/// NOTE: It is expected that the PWM [`Clock`] is brought up before using any of the functionality
+/// of a channel.
 ///
 /// [`Clock`]: ../car/struct.Clock.html
 #[derive(Debug)]
 pub struct PwmChannel {
-    /// A pointer to the PWM [`Registers`] block.
-    ///
-    /// [`Registers`]: struct.Registers.html
     registers: *const Registers,
 }
 
 // Definitions of known PWM channels.
 
 impl PwmChannel {
+    /// Representation of the PWM 0 channel.
     pub const PWM_0: Self = PwmChannel {
         registers: PWM_0_REGISTERS,
     };
 
+    /// Representation of the PWM 1 channel.
     pub const PWM_1: Self = PwmChannel {
         registers: PWM_0_REGISTERS,
     };
 
+    /// Representation of the PWM 2 channel.
     pub const PWM_2: Self = PwmChannel {
         registers: PWM_0_REGISTERS,
     };
 
+    /// Representation of the PWM 3 channel.
     pub const PWM_3: Self = PwmChannel {
         registers: PWM_0_REGISTERS,
     };
 }
 
 impl PwmChannel {
-    /// Enables pulse generation through this channel.
+    /// Enables pulse generation mechanism of this channel.
     pub fn enable(&self) {
         let controller = unsafe { &*self.registers };
 
@@ -60,7 +60,7 @@ impl PwmChannel {
             .modify(PWM_CONTROLLER_PWM_CSR_0::ENB::SET);
     }
 
-    /// Disables pulse generation through this channel.
+    /// Disables pulse generation mechanism of this channel.
     pub fn disable(&self) {
         let controller = unsafe { &*self.registers };
 
@@ -71,8 +71,8 @@ impl PwmChannel {
 
     /// Configures the pulse width of the channel.
     ///
-    /// The argument is the desired duty cycle as a float value,
-    /// representing a percentage ranging from 0.0 (0%) to 1.0 (100%).
+    /// The argument is the desired duty cycle as a float value, representing a percentage
+    /// ranging from 0.0 (0%) to 1.0 (100%).
     pub fn set_pulse_width(&self, duty: f32) -> Result<(), ()> {
         let controller = unsafe { &*self.registers };
 
